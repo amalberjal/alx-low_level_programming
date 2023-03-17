523e4ce776d4f53edb586c2e44cc8b80d3640459
amal berjal <berjalamal2@gmail.com>
2023-03-17 15:55:33 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the alphabet in lowercase,
+ * and then in uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int low = 97;
+	int up = 65;
+
+	while (low < 123)
+	{
+		putchar ((char) low);
+		low++;
+	}
+	while (up < 91)
+	{
+		putchar ((char) up);
+		up++;
+	}
+	putchar('\n');
+
+	return (0);
+}
